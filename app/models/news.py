0effb746d0cd8752be9c29cda7bea400c3dfefe0
Ahonoryoshi <yoshiahona@gmail.com>
2022-05-05 10:04:24 +0300

    create a news class for creating news objects

---
+++ app/models/news.py
@@ -0,0 +1,16 @@
+class News:
+    """
+    Class for creating instances of news objects
+    """
+    
+    def __init__(self,author, title, description, url, urlToImage, publishedAt):
+        """
+        This method allows us to instantiate an instance.
+        """
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+
