096898219e402db4e624dec12d9cc84aa917d5c6
Ahonoryoshi <yoshiahona@gmail.com>
2022-05-05 10:32:00 +0300

    add code to get news sources

--- app/config.py
+++ app/config.py
@@ -13,7 +13,10 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    NEWS_API_BASE_URL = "https://newsapi.org/v2/sources?apiKey={}"
+    EVERYTHING_BASE_API_URL = "https://newsapi.org/v2/everything?domains=wsj.com&apikey={}"
+    TOP_HEADLINES_BASE_API_URL = 'https://newsapi.org/v2/top-headlines?sources={}&apiKey={}'
+    BUSINESS_TOP_HEADLINES = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey={}"
 
 
 class DevConfig(Config):
